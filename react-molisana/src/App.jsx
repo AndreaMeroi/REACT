4db20e12b8ab3dca40c9937d5485577bc6d2fa91
AndreaMeroi <andreameroidublin@gmail.com>
2026-04-22 15:18:38 +0200

    add till COMPONENTI

--- react-molisana/src/App.jsx
+++ react-molisana/src/App.jsx
@@ -1,18 +1,54 @@
 
 import reactLogo from './assets/react.svg'
+import molisanaLogo from './assets/img/logo.png'
 import viteLogo from './assets/vite.svg'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <header>
         <img src={molisanaLogo} alt="" />
+        <nav>
+          <a href="" className='active' >Home </a>
+          <a href="">Prodotti </a>
+          <a href="">Chi Siamo </a>
+          <a href="">Contatti</a>
+        </nav>
+
       </header>
-      <main><h1>Hellow world!</h1></main>
-      <footer></footer>
+      <main>
+
+      </main>
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <img src={molisanaLogo} alt="" />            </div>
+            <div className="col">
+              <h3>Pastificio</h3>
+              <ul>
+                <li><a href="">IL pastificio</a></li>
+                <li><a href="">IL pastificio</a></li>
+                <li><a href="">IL pastificio</a></li>
+                <li><a href="">IL pastificio</a></li>
+                <li><a href="">IL pastificio</a></li>
+              </ul>
+            </div>
+            <div className="col">
+              <h3>Prodotti</h3>
+              <ul>
+                <li><a href="">Prodotti</a></li>
+                <li><a href="">Prodotti</a></li>
+                <li><a href="">Prodotti</a></li>
+                <li><a href="">Prodotti</a></li>
+                <li><a href="">Prodotti</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
 
     </>
   )
