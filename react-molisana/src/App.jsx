6a6f71e721cc26d4a331e67d5f1e6917179270d4
AndreaMeroi <andreameroidublin@gmail.com>
2026-04-22 14:31:23 +0200

    add react intro

---
+++ react-molisana/src/App.jsx
@@ -0,0 +1,21 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        <img src={molisanaLogo} alt="" />
+      </header>
+      <main><h1>Hellow world!</h1></main>
+      <footer></footer>
+
+    </>
+  )
+}
+
+export default App
