1997529fa2bcd296b44747be171f0f2d64a0ae37
AndreaMeroi <andreameroidublin@gmail.com>
2026-05-06 18:33:04 +0200

    ad last modification missing 2 last points

--- react-use-state/src/components/button.jsx
+++ react-use-state/src/components/button.jsx
@@ -33,8 +33,23 @@ export default function Button({ languages }) {
                         ))}
 
                 </div>
+                <div className="cardShower">
 
-                <Card />
+                    {selectedLanguage ? (
+                        // Se selectedLanguage NON è null, mostra la card selezionata
+                        <div className="card" key={selectedLanguage.id}>
+                            <h5 className="card-title">{selectedLanguage.title}</h5>
+                            <p className="card-text">{selectedLanguage.description}</p>
+                        </div>
+                    ) :
+                        // Altrimenti (se è null), mostra un messaggio o nulla
+                        (<p> nessun linguaggio selezionato </p>)
+
+
+
+                    }
+
+                </div>
 
             </div>
 
