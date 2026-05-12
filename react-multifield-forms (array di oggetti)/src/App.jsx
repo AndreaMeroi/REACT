d399d8964e77f01f60793aa2d35e73d109efab4e
AndreaMeroi <andreameroidublin@gmail.com>
2026-05-12 18:26:19 +0200

    add handleChange in multifield form oggetti

--- react-multifield-forms (array di oggetti)/src/App.jsx
+++ react-multifield-forms (array di oggetti)/src/App.jsx
@@ -39,6 +39,34 @@ function App() {
     //devo mettere task tra le quadre, perchè map è un metodo degli array 
     setTasks([task, ...tasks])
 
+    // per svuotare il form dopo averlo inviato 
+    setFormData(initialFormData)
+
+  }
+
+  /* 
+  nella funzione handleChange posso racchiudere nella funzione la parte di codice
+  (e) => setFormData({ ...formData, key: e.target.value }) 
+  e passare ad onChange direttamente handleChange lasciando il codice molto più ordinato 
+  */
+  function handleChange(e) {
+    /*
+       console.log(e.target);
+    console.log(e.target.name);
+    console.log(e.target.type);
+
+    */
+
+    // creo la variabile value per adattare la risposta anche agli elementi che non hanno il value, come checkbox
+    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value
+    console.log(value);
+
+    // creo dinamicamente la chiave mettendo tra le quadre il name dell'elemento
+    console.log({ ...formData, [e.target.name]: e.target.value });
+
+    //attivo il setter per modificare la UI 
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+
 
   }
 
@@ -50,57 +78,7 @@ function App() {
 
     setTasks(filteredTasks)
 
-    //2 OPZIONI PER ATTIVARE LA MODIFICA DELL'UI SENZA CREARE LA VARIABILE filteredTasks
-
-    //1> setTask(tasks.filter((task,index)=> index != i))
-    //2> setTask((currentTasks)=> currentTasks.filter((task,index)=> index != i))
-
   }
-
-  /*
-    // funzione per modificare la UI aggiungendo una nuova task
-  function handleSubmit(e) {
-    e.preventDefault() // indispensabile per prevenire il caricameto automatico della pagina 
-    console.log('Clicked Submit');
-
-    console.log(newTask, newTask.length);
-    if (newTask.length > 5) {
-      //update tasks
-      console.log('You can update the tasks');
-      //per clonare il vecchio state aggiungendo la nuova task e quindi aggiornare la UI utilizzo l'operatore spread(...)
-      setTask([newTask, ...tasks])
-      //per svuotare il compo dell'input 
-      setNewTask('')
-
-    } else {
-      alert('Tasks must be at least 5 characters long')
-    }
-
-
-  }
-  */
-
-
-  /*
-  // funzione per far funzionare il bottone trash 
-  function handleTrash(i) {
-    console.log('clicked to trash with the index:', i);
-
-    //devo creare un nuovo array per modificare lo stato, utilizzo filter 
-    // se index è diverso da i allora lo voglio nelle filteredTasks, quindi la task cliccata non sarà più nel nuovo array
-    const filteredTasks = tasks.filter((task, index) => index != i)
-    console.log(filteredTasks,);
-    //uso setTasks per modificare la UI
-    setTask(filteredTasks)
-
-    //2 OPZIONI PER ATTIVARE LA MODIFICA DELL'UI SENZA CREARE LA VARIABILE filteredTasks
-
-    //1> setTask(tasks.filter((task,index)=> index != i))
-    //2> setTask((currentTasks)=> currentTasks.filter((task,index)=> index != i))
-
-  }
-  */
-
 
   return (
     <>
@@ -156,12 +134,17 @@ function App() {
                     {/*anzichè onChange={(e) => setNomeSetter(e.target.value )} utilizzo un setter unico,
  ma nella funzione vado a spalmare per ogni elelmento del form il suo contenuto utilizzando 
  il setter spreddando ogni volta il nuovo elemento nell'intero oggetto,{(e) => setFormData({ ...formData, title: e.target.value })} 
- in questo caso title e per questo il contenuto delle tonde va tra le graffe*/}
+ in questo caso title e per questo il contenuto delle tonde va tra le graffe
+ 
+  <input name='title' id='title' className='form-control' type="text" placeholder='type a task to do' value={formData.title} onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
+ */}
 
-                    <input name='title' id='title' className='form-control' type="text" placeholder='type a task to do' value={formData.title} onChange={(e) => setFormData({ ...formData, title: e.target.value })} />
+                    <input name='title' id='title' className='form-control' type="text" placeholder='type a task to do' value={formData.title} onChange={handleChange} />
+
                   </div>
                   <div className='mb-3'>
-                    <select className='form-select' id='priority' name='priority' value={formData.priority} onChange={(e) => setFormData({ ...formData, priority: e.target.value })}>
+                    {/*<select className='form-select' id='priority' name='priority' value={formData.priority} onChange={(e) => setFormData({ ...formData, priority: e.target.value })}>*/}
+                    <select className='form-select' id='priority' name='priority' value={formData.priority} onChange={handleChange}>
                       <option value="low">low</option>
                       <option value="mid">mid</option>
                       <option value="high">high</option>
@@ -170,18 +153,21 @@ function App() {
 
 
                   <div className="mb-3">
-                    <textarea className='form-control' name="description" id="description" value={formData.description} onChange={(e) => setFormData({ ...formData, description: e.target.value })} rows="3" placeholder='describe yuor task...'></textarea>
+                    {/* <textarea className='form-control' name="description" id="description" value={formData.description} onChange={(e) => setFormData({ ...formData, description: e.target.value })} rows="3" placeholder='describe yuor task...'></textarea>*/}
+                    <textarea className='form-control' name="description" id="description" value={formData.description} onChange={handleChange} rows="3" placeholder='describe yuor task...'></textarea>
                   </div>
 
                   {/*nel checkbox non c'è .value nel setter, ma .checked che restituisce true o false in funzione che sia checked o meno*/}
 
                   <div className="form-check mb-3">
+
                     <input
                       type="checkbox"
                       className='form-check-input'
                       id='due_today'
                       name='due_today'
-                      onChange={(e) => setFormData({ ...formData, due_today: e.target.checked })}
+                      /* onChange={(e) => setFormData({ ...formData, due_today: e.target.checked })} */
+                      onChange={handleChange}
                     />
                     <label className="form-check-label" htmlFor=''>due today</label>
 
@@ -199,18 +185,7 @@ function App() {
 
 
           </div>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
+        </div >
 
       </div >
 
